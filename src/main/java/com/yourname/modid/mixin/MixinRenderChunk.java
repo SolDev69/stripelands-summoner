d7d1c9d80e04bfea2425c744415b9e7781765d48
MicherGit <40839581+MicherGit@users.noreply.github.com>
2021-05-26 12:51:05 -0400

    fix z fighting i think

--- src/main/java/com/yourname/modid/mixin/MixinRenderChunk.java
+++ src/main/java/com/yourname/modid/mixin/MixinRenderChunk.java
@@ -19,50 +19,7 @@ import java.nio.FloatBuffer;
 
 @SideOnly(Side.CLIENT)
 @Mixin(RenderChunk.class)
-public abstract class MixinRenderChunk {
-    @Shadow
-    private final FloatBuffer modelviewMatrix = GLAllocation.createDirectFloatBuffer(16);
-    @Shadow
-    private final BlockPos.MutableBlockPos position = new BlockPos.MutableBlockPos(-1, -1, -1);
-
-    @Shadow
-    public abstract void stopCompileTask();
-    @Shadow
-    public AxisAlignedBB boundingBox;
-    @Shadow
-    private final BlockPos.MutableBlockPos[] mapEnumFacing = new BlockPos.MutableBlockPos[6];
-    @Shadow
-    private void initModelviewMatrix()
-    {
-        GlStateManager.pushMatrix();
-        GlStateManager.loadIdentity();
-        float f = 1.000001F;
-        GlStateManager.translate(-8.0F, -8.0F, -8.0F);
-        GlStateManager.scale(1.000001F, 1.000001F, 1.000001F);
-        GlStateManager.translate(8.0F, 8.0F, 8.0F);
-        GlStateManager.getFloat(2982, this.modelviewMatrix);
-        GlStateManager.popMatrix();
-    }
-    /**
-     * @author
-     */
-    @Overwrite
-    public void setPosition(int x, int y, int z)
-    {
-        if (x != this.position.getX() || y != this.position.getY() || z != this.position.getZ())
-        {
-            this.stopCompileTask();
-            this.position.setPos(x, y, z);
-            this.boundingBox = new AxisAlignedBB((float)x, (float)y, (float)z, (float)(x + 16), (float)(y + 16), (float)(z + 16));
-
-            for (EnumFacing enumfacing : EnumFacing.values())
-            {
-                this.mapEnumFacing[enumfacing.ordinal()].setPos(this.position).move(enumfacing, 16);
-            }
-
-            this.initModelviewMatrix();
-        }
-    }
+public class MixinRenderChunk {
     /**
      * @author
      */
@@ -70,6 +27,6 @@ public abstract class MixinRenderChunk {
     private void preRenderBlocks(BufferBuilder bufferBuilderIn, BlockPos pos)
     {
         bufferBuilderIn.begin(7, DefaultVertexFormats.BLOCK);
-        bufferBuilderIn.setTranslation((double)(-pos.getX()), (double)(-pos.getY()), (double)(-pos.getZ()));
+        bufferBuilderIn.setTranslation((float)(-pos.getX()), (float)(-pos.getY()), (float)(-pos.getZ()));
     }
 }
