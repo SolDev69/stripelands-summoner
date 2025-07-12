5432ef702cb0aacc55209988a3db068503bfceee
SolDev <michaelcraft1104@gmail.com>
2025-07-12 11:55:56 -0400

    work done

--- src/main/java/com/yourname/modid/mixin/MixinBlockModelRenderer.java
+++ src/main/java/com/yourname/modid/mixin/MixinBlockModelRenderer.java
@@ -11,6 +11,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.client.model.pipeline.LightUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
@@ -86,4 +87,57 @@ public abstract class MixinBlockModelRenderer {
             buffer.putPosition(d0, d1, d2);
         }
     }
+    
+    
+    @Overwrite
+    private void renderQuadsFlat(IBlockAccess p_187496_1_, IBlockState p_187496_2_, BlockPos p_187496_3_, int p_187496_4_, boolean p_187496_5_, BufferBuilder p_187496_6_, List<BakedQuad> p_187496_7_, BitSet p_187496_8_) {
+        Vec3d vec3d = p_187496_2_.getOffset(p_187496_1_, p_187496_3_);
+        float d0 = (float)p_187496_3_.getX() + (float)vec3d.x;
+        float d1 = (float)p_187496_3_.getY() + (float)vec3d.y;
+        float d2 = (float)p_187496_3_.getZ() + (float)vec3d.z;
+        int i = 0;
+
+        for(int j = p_187496_7_.size(); i < j; ++i) {
+            BakedQuad bakedquad = (BakedQuad)p_187496_7_.get(i);
+            if (p_187496_5_) {
+                this.fillQuadBounds(p_187496_2_, bakedquad.getVertexData(), bakedquad.getFace(), (float[])null, p_187496_8_);
+                BlockPos blockpos = p_187496_8_.get(0) ? p_187496_3_.offset(bakedquad.getFace()) : p_187496_3_;
+                p_187496_4_ = p_187496_2_.getPackedLightmapCoords(p_187496_1_, blockpos);
+            }
+
+            p_187496_6_.addVertexData(bakedquad.getVertexData());
+            p_187496_6_.putBrightness4(p_187496_4_, p_187496_4_, p_187496_4_, p_187496_4_);
+            if (bakedquad.hasTintIndex()) {
+                int k = this.blockColors.colorMultiplier(p_187496_2_, p_187496_1_, p_187496_3_, bakedquad.getTintIndex());
+                if (EntityRenderer.anaglyphEnable) {
+                    k = TextureUtil.anaglyphColor(k);
+                }
+
+                float f = (float)(k >> 16 & 255) / 255.0F;
+                float f1 = (float)(k >> 8 & 255) / 255.0F;
+                float f2 = (float)(k & 255) / 255.0F;
+                if (bakedquad.shouldApplyDiffuseLighting()) {
+                    float diffuse = LightUtil.diffuseLight(bakedquad.getFace());
+                    f *= diffuse;
+                    f1 *= diffuse;
+                    f2 *= diffuse;
+                }
+
+                p_187496_6_.putColorMultiplier(f, f1, f2, 4);
+                p_187496_6_.putColorMultiplier(f, f1, f2, 3);
+                p_187496_6_.putColorMultiplier(f, f1, f2, 2);
+                p_187496_6_.putColorMultiplier(f, f1, f2, 1);
+            } else if (bakedquad.shouldApplyDiffuseLighting()) {
+                float diffuse = LightUtil.diffuseLight(bakedquad.getFace());
+                p_187496_6_.putColorMultiplier(diffuse, diffuse, diffuse, 4);
+                p_187496_6_.putColorMultiplier(diffuse, diffuse, diffuse, 3);
+                p_187496_6_.putColorMultiplier(diffuse, diffuse, diffuse, 2);
+                p_187496_6_.putColorMultiplier(diffuse, diffuse, diffuse, 1);
+            }
+
+            p_187496_6_.putPosition(d0, d1, d2);
+        }
+
+    }
+
 }
